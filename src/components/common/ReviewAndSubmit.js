aa4858c5bb9eb2d78574897508775c38332933eb
cpleasants <chris.pleasants@generalassemb.ly>
2025-04-26 10:31:12 -0400

    improved the Review and Submit page with MUI

--- src/components/common/ReviewAndSubmit.js
+++ src/components/common/ReviewAndSubmit.js
@@ -5,72 +5,116 @@ import FloatingFooter from './styles/floatingFooter.style';
 
 const ReviewAndSubmit = ({formData, restart, handleApiResponse}) => {
 
-    const checkedBoxes = Object.keys(formData).filter(c => formData[c] === true)
+  const checkedBoxes = Object.keys(formData).filter(c => formData[c] === true)
     
-    const generateRequest = () => {
-        return {
-          "hard_consonants" : phonemes["hard_consonants"].filter(s => checkedBoxes.includes(`l_${s}`)),
-          "soft_consonants" : checkedBoxes.includes("allow_soft_consonants") || checkedBoxes.includes("cvce") 
-            ? phonemes["soft_consonants"].filter(s => checkedBoxes.includes(s)) : [],
-          "short_vowels" : phonemes["short_vowels"].filter(s => checkedBoxes.includes(s)),
-          "long_vowels" : checkedBoxes.includes("allow_long_vowels") || checkedBoxes.includes("cvce") 
-            ? phonemes["long_vowels"].filter(s => checkedBoxes.includes(s)) : [],
-          "vowel_teams" : phonemes["vowel_teams"].filter(s => checkedBoxes.includes(s)),
-          "digraphs" : phonemes["digraphs"].filter(s => checkedBoxes.includes(s)),
-          "double_letters" : checkedBoxes.includes("allow_double_consonants") 
-            ? phonemes["double_letters"].filter(s => checkedBoxes.includes(s[0])) : [],
-          "prefix_digraphs" : phonemes["prefix_digraphs"].filter(s => checkedBoxes.includes(s)),
-          "prefix_blends" : phonemes["prefix_blends"].filter(s => checkedBoxes.includes(s)),
-          "suffix_blends" : phonemes["suffix_blends"].filter(s => checkedBoxes.includes(s)),
-          "common_endings" : phonemes["common_endings"].filter(s => checkedBoxes.includes(s)),
-          "allow_silent_e" : checkedBoxes.includes("allow_silent_e"),
-          "allow_vc" : checkedBoxes.includes("vc"),
-          "allow_cvc" : checkedBoxes.includes("cvc"),
-          "allow_cvce" : checkedBoxes.includes("cvce"),
-          "allow_cvcvc" : checkedBoxes.includes("cvcvc"),
-          "decodable_only" : true, // TODO: how to include non-decodable words? Should I even?
-          "sight_words" : Object.values(sightWordSets).flat().filter(w => checkedBoxes.includes(w))
-        }
+  const generateRequest = () => {
+    return {
+      "hard_consonants" : phonemes["hard_consonants"].filter(s => checkedBoxes.includes(`l_${s}`)),
+      "soft_consonants" : checkedBoxes.includes("allow_soft_consonants") || checkedBoxes.includes("cvce") 
+        ? phonemes["soft_consonants"].filter(s => checkedBoxes.includes(s)) : [],
+      "short_vowels" : phonemes["short_vowels"].filter(s => checkedBoxes.includes(s)),
+      "long_vowels" : checkedBoxes.includes("allow_long_vowels") || checkedBoxes.includes("cvce") 
+        ? phonemes["long_vowels"].filter(s => checkedBoxes.includes(s)) : [],
+      "vowel_teams" : phonemes["vowel_teams"].filter(s => checkedBoxes.includes(s)),
+      "digraphs" : phonemes["digraphs"].filter(s => checkedBoxes.includes(s)),
+      "double_letters" : checkedBoxes.includes("allow_double_consonants") 
+        ? phonemes["double_letters"].filter(s => checkedBoxes.includes(s[0])) : [],
+      "prefix_digraphs" : phonemes["prefix_digraphs"].filter(s => checkedBoxes.includes(s)),
+      "prefix_blends" : phonemes["prefix_blends"].filter(s => checkedBoxes.includes(s)),
+      "suffix_blends" : phonemes["suffix_blends"].filter(s => checkedBoxes.includes(s)),
+      "common_endings" : phonemes["common_endings"].filter(s => checkedBoxes.includes(s)),
+      "allow_silent_e" : checkedBoxes.includes("allow_silent_e"),
+      "allow_vc" : checkedBoxes.includes("vc"),
+      "allow_cvc" : checkedBoxes.includes("cvc"),
+      "allow_cvce" : checkedBoxes.includes("cvce"),
+      "allow_cvcvc" : checkedBoxes.includes("cvcvc"),
+      "decodable_only" : true, // TODO: how to include non-decodable words? Should I even?
+      "sight_words" : Object.values(sightWordSets).flat().filter(w => checkedBoxes.includes(w))
+    }
+  }
+
+  const data = generateRequest();    
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // allows me to handle the <form >submission manually instead of going to default (which is redirect to URL)
+
+    try {
+      const res = await fetch('http://localhost:8000/filter-words', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), 
+      });
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
       }
 
-      const data = generateRequest();    
-    
-      const handleSubmit = async (event) => {
-        event.preventDefault(); // allows me to handle the <form >submission manually instead of going to default (which is redirect to URL)
-    
-        try {
-          const res = await fetch('http://localhost:8000/filter-words', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data), 
-          });
-          if (!res.ok) {
-            throw new Error('Network response was not ok');
-          }
-    
-          const result = await res.json();
-          handleApiResponse(result);
-          
-        } catch (error) {
-          console.error('Error submitting data:', error);
-        }
-      }
+      const result = await res.json();
+      handleApiResponse(result);
+      
+    } catch (error) {
+      console.error('Error submitting data:', error);
+    }
+  }
 
+  const listReviewFields = [
+    // Note that I'm handling hard_consonants separately
+    { label: "Soft Consonants", key: "soft_consonants" },
+    { label: "Short Vowels", key: "short_vowels" },
+    { label: "Long Vowels", key: "long_vowels" },
+    { label: "Vowel Teams", key: "vowel_teams" },
+    { label: "Digraphs", key: "digraphs" },
+    { label: "Double Letters", key: "double_letters" },
+    { label: "Prefix Digraphs", key: "prefix_digraphs" },
+    { label: "Prefix Blends", key: "prefix_blends" },
+    { label: "Suffix Blends", key: "suffix_blends" },
+    { label: "Common Endings", key: "common_endings" },
+    { label: "Sight Words", key: "sight_words" }
+  ]
 
+  const displayListReviewFields = (field) => {
     return (
-        <Box>
-            <Typography variant="h4">Review</Typography>
-            <Typography variant="body1" component="pre">
-              {JSON.stringify(data, null, 2)}
-            </Typography>
-            <FloatingFooter>
-              <Button  variant="contained" onClick={restart}>Edit</Button>
-              <Button  variant="contained" onClick={handleSubmit}>Submit</Button>
-            </FloatingFooter>
-        </Box>
+      <Typography variant="body1" gutterBottom>
+        <strong>{field.label}: </strong>
+          {`${data[field.key].length === 0 ?  "None" : data[field.key].join(', ')}`}
+      </Typography>
     )
+  }
+
+  const booleanReviewFields = [
+    { label: "Allow Silent E", key: "allow_silent_e" },
+    { label: "Allow VC", key: "allow_vc" },
+    { label: "Allow CVC", key: "allow_cvc" },
+    { label: "Allow CVCE", key: "allow_cvce" },
+    { label: "Allow CVCVC", key: "allow_cvcvc" },
+    { label: "Decodable Only", key: "decodable_only" }
+  ]
+
+  const displayBooleanReviewFields = (field) => {
+    return (
+      <Typography variant="body1" gutterBottom>
+        <strong>{field.label}: </strong>
+          {data[field.key] ? "Yes" : "No"}
+      </Typography>
+    )
+  }
+
+  return (
+    <Box>
+      <Typography variant="h4">Review</Typography>
+      <Typography variant="body1" gutterBottom>
+        <strong>Hard Consonants: </strong>
+          {`${data['hard_consonants'].length === 20 ?  "All (except q)" : data['hard_consonants'].join(', ')}`}
+      </Typography>
+      {listReviewFields.map(field => displayListReviewFields(field))}
+      {booleanReviewFields.map(field => displayBooleanReviewFields(field))}
+      <FloatingFooter>
+        <Button  variant="contained" onClick={restart}>Edit</Button>
+        <Button  variant="contained" onClick={handleSubmit}>Submit</Button>
+      </FloatingFooter>
+    </Box>
+  )
 }
 
 export default ReviewAndSubmit;
