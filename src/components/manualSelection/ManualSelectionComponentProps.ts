f0b6419b3935ac7297f60e7100b6a2bd9ee97e8b
cpleasants <chris.pleasants@generalassemb.ly>
2025-05-15 17:34:09 -0700

    expanded on TODO to come back to later

--- src/components/manualSelection/ManualSelectionComponentProps.ts
+++ src/components/manualSelection/ManualSelectionComponentProps.ts
@@ -5,4 +5,4 @@ interface ManualSelectionComponentProps {
 }
 
 export default ManualSelectionComponentProps;
-// TODO: is this the right place to put this file, and the right way to do this in general?
+// TODO: is this the right place to put this file, and the right way to do this in general? Should I have an even more general, common prop for all the children of the main app since they will all take the selected and setSelected? Should I even make that some kind of object in and of itself? 
